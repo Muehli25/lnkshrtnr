768ea995ff1955ba01866ff0b7dd955cf658614e
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-07-20 00:09:00 +0200

    add db connection

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express = require('express');
+
+
 var app = express();
 
+const dbConnection = require("./db.connector")
+const Links = dbConnection.links;
+const Op = dbConnection.Sequelize.Op;
+
+dbConnection.sequelize.sync();
+
 app.get('/', function (req, res) {
+    dbConnection
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+    dbConnection.link.create()
     res.send('Hello World!');
 });
 
---
+++ link.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, Sequelize) => {
+    return sequelize.define("link", {
+        shortValue: {
+            type: Sequelize.STRING
+        },
+        longValue: {
+            type: Sequelize.STRING
+        },
+        public: {
+            type: Sequelize.BOOLEAN
+        }
+    });
+};
