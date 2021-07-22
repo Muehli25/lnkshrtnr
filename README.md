f6c772a26f70f7fbffcaec485442ded1b745e561
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-07-22 23:32:38 +0200

    Add docker compose for easy deployment

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This small application is written in javscript and provides a very simple link s
 - [x] Create Links with a random 6 string long identifier
 - [x] Resolve a the string into a actual url, which will automatically redirect to the saved site.
 - [x] Save data in a sql database (in this case a mariadb)
-- [ ] Easy way to deploy with Docker
+- [x] Easy way to deploy with Docker
 
 ## Future Features
 
--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,33 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "example",
-    DB: "lnkshrt",
-    dialect: "mariadb",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+module.exports = function (){
+    switch(process.env.NODE_ENV) {
+        case 'production':
+            console.log("running in production mode");
+            return {
+                HOST: "db",
+                USER: "root",
+                PASSWORD: "example",
+                DB: "lnkshrt",
+                dialect: "mariadb",
+                pool: {
+                    max: 5,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000
+                }
+            }
+        default:
+            return {
+                HOST: "localhost",
+                USER: "root",
+                PASSWORD: "example",
+                DB: "lnkshrt",
+                dialect: "mariadb",
+                pool: {
+                    max: 5,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000
+                }
+            }
     }
   };
